2912da43a707d3d15c56be0f4b7f1d6f3d8d5108
Vinicius Herculano <viniciusherculanomap@gmail.com>
2021-01-28 21:25:59 -0300

    Fase02_7 Finalizada

--- Fase02/scripts.js
+++ Fase02/scripts.js
@@ -3,10 +3,14 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards){
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id");
         modal0verlay.classList.add('active')
+        modal0verlay.querySelector("iframe").src = `https://youtube.com.br/embed/${videoId}`
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modal0verlay.classList.remove("active")
+    modal0verlay.querySelector("iframe").src = ""
 })
+
